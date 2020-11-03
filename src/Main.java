954473236927406a447baab1a2bc8a9286958fe8
unknown <landenoooo@gmail.com>
2020-11-03 15:49:28 -0600

    Added gameloop to main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@
  *  --module-path "javafx-sdk-11.0.2\lib" --add-modules javafx.controls
  */
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -23,19 +24,26 @@ public class Main extends Application{
 	@Override
 	public void start(Stage stage) throws Exception {
 		// Setup window and scene
-		stage.setTitle("Smart Car");
+		stage.setTitle("Smart-Car");
         stage.setResizable(false);
-        
         Group root = new Group();
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
-		
         Canvas canvas = new Canvas(WIDTH, HEIGHT);
         root.getChildren().add(canvas);
-        stage.show();
         
+        // This is passed to everything we want to draw on screen ex: draw(ex)
         GraphicsContext gc = canvas.getGraphicsContext2D();
+                
+		Background bg = new Background("images/backgrounds/road.png", 5, HEIGHT);
+        
+        new AnimationTimer() {
+        	public void handle(long currentNanoTime) {
+        		bg.draw(gc);
+        	}
+        }.start();
+        
+        stage.show();
 	}
 
 }
